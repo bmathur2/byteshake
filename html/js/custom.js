5a2e8ab6dc8da842027bd8d16335b71f09543ed4
Banti Mathur <bmathur2@gmail.com>
2021-09-15 11:59:20 +0530

    html code added

--- html/js/custom.js
+++ html/js/custom.js
@@ -68,3 +68,29 @@ jQuery(document).ready(function($) {
 		            }
 		        });
         	});
+			
+			
+			
+			
+$(document).ready(function(){
+  $('.bf-testimonial-slick').slick({
+		pauseOnHover: true,
+		autoplay: false,
+		autoplayspeed: 2000,
+		speed: 1000,
+		centerMode: true,
+		centerPadding: '20%',
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		arrows: true,
+		dots: true,
+		draggable:true,
+		responsive: [{
+			breakpoint: 991,
+			settings: {
+				slidesToShow: 1,
+			}
+		}]
+    
+  });
+});
